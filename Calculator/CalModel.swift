ca616678ffe8c9912e2fdabef90c6cc8682d5df3
David <k7tai@uwaterloo.ca>
2017-09-26 17:31:11 -0400

    display bug

---
+++ Calculator/CalModel.swift
@@ -0,0 +1,89 @@
+//
+//  CalModel.swift
+//  Calculator
+//
+//  Created by David on 2017-09-26.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+import Foundation
+// helper functions
+func changeSign(num: Double) -> Double{
+    return -num
+}
+func multiply(num1: Double, num2: Double) -> Double{
+    return num1 * num2
+}
+
+struct CalModel{
+    
+    // functions
+    private enum functions{
+        case constant(Double)
+        case unaryfun((Double) -> Double)
+        case binaryfun((Double,Double)-> Double)
+        case equal
+    }
+    // operations
+    private var ops: Dictionary<String,functions> = [
+    "π": functions.constant(Double.pi),
+    "e": functions.constant(M_E),
+    "±": functions.unaryfun(-),
+    "cos": functions.unaryfun(cos),
+    "sin": functions.unaryfun(sin),
+    "√": functions.unaryfun(sqrt),
+    "*": functions.binaryfun(multiply),
+    "+": functions.binaryfun(+),
+    "=": functions.equal
+    ]
+    // accumulator for calculator
+    private var accumulator: Double?
+    
+    // perform op
+    mutating func Operations(_ op: String){
+        if let operations = ops[op]{
+            switch operations{
+            case .constant(let value):
+                accumulator = value
+            case .unaryfun(let fun):
+                if accumulator != nil{
+                    accumulator = fun(accumulator!)
+                }
+            case .binaryfun(let fun):
+                if accumulator != nil{
+                    PendingBOP = PendingBinaryOP(function: fun,firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equal:
+                equals()
+                
+            }
+        }
+    }
+    // for binary operations
+    mutating private func equals(){
+        if PendingBOP != nil && accumulator != nil{
+            accumulator = PendingBOP?.perform(with: accumulator!)
+            PendingBOP = nil
+        }
+        
+    }
+    private var PendingBOP: PendingBinaryOP?
+    
+    private struct PendingBinaryOP{
+        let function: (Double,Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double)-> Double{
+            return function(firstOperand,secondOperand)
+        }
+    }
+    mutating func setOperand(_ operand: Double){
+        accumulator = operand
+    }
+    var result: Double?{
+        get{
+            return accumulator
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // display
+    @IBOutlet weak var display: UILabel!
+    
+    // CalModel
+    private var calmodel = CalModel()
+    
+    // 0-9, 10 buttons
+    @IBAction func numbers(_ sender: UIButton) {
+        if let touched = sender.currentTitle{
+            if !(Int(displayvalue) == 0 && Int(touched) == 0){
+                display!.text = String(displayvalue) + touched
+            }
+        }
+        else{
+            print("ERROR: numbers title not set")
+        }
+    }
+    // get/set display
+    var displayvalue: Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    // operations
+    @IBAction func Operations(_ sender: UIButton) {
+        calmodel.setOperand(displayvalue)
+        if let op = sender.currentTitle{
+            calmodel.Operations(op)
+        }
+        if let result = calmodel.result{
+            displayvalue = result
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
 
 }
 
